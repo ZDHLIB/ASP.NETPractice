5f48d54fb3d4757a720c0cfdb2b67aa1edc5262b
dzhong <dzhong@ciena.com>
2018-06-20 16:49:05 -0400

    Add middleware

--- OdeToFood/IGreeter.cs
+++ OdeToFood/IGreeter.cs
@@ -1,5 +1,19 @@
-﻿namespace OdeToFood {
+﻿using Microsoft.Extensions.Configuration;
+
+namespace OdeToFood {
     public interface IGreeter {
         string GetMessage();
+    }
+
+    public class Greeter : IGreeter {
+        private IConfiguration _configuration;
+
+        public Greeter(IConfiguration configuration) {
+            _configuration = configuration;
+        }
+
+        public string GetMessage() {
+            return _configuration["Greeting"];
+        }
     }
 }
--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace OdeToFood
 {
@@ -16,18 +17,40 @@ namespace OdeToFood
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IGreeter, Greeter>();
+            services.AddTransient<IGreeter, Greeter>();  // Lazy creation, create instance when needed
+            services.AddScoped<IGreeter, Greeter>(); // Create a instance per http request.
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, 
                               IHostingEnvironment env,
                               //IConfiguration configuration,
-                              IGreeter greeter)
+                              IGreeter greeter,
+                              ILogger<Startup> logger)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.Use(next => {
+                // middleware, run once when start up
+                return async context => {
+                    logger.LogInformation("Request incoming");
+                    if (context.Request.Path.StartsWithSegments("/start")) {
+                        await context.Response.WriteAsync("Hit!!");
+                        logger.LogInformation("Request handled");
+                    } else {
+                        await next(context);
+                        logger.LogInformation("Request outgoing");
+                    }
+                };
+            });
+
+            app.UseWelcomePage(new WelcomePageOptions {
+                Path = "/pathPattern"
+            });
 
             app.Run(async (context) =>
             {
