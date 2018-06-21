aacff3db109b88b5aa9d0d9bcc60b28592b5bbfe
dzhong <dzhong@ciena.com>
2018-06-21 10:43:21 -0400

    add models, view list of models at front

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using OdeToFood.Models;
+using OdeToFood.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +10,15 @@ namespace OdeToFood.Controllers
 {
     public class HomeController : Controller
     {
+        private IResturantData _resturantData;
+        public HomeController(IResturantData restaurantData) {
+            _resturantData = restaurantData;
+        }
         public IActionResult Index() {
-            var model = new Restaurant { Id = 1, Name = "Zac" };
-            return new ObjectResult(model);
+            //var model = new Restaurant { Id = 1, Name = "Zac" };
+            //return new ObjectResult(model);
+            var model = _resturantData.GetAll();
+            return View(model);
         }
     }
 }
---
+++ OdeToFood/Services/RestaurantService.cs
@@ -0,0 +1,27 @@
+﻿using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Services
+{
+    public class RestaurantService : IResturantData
+    {
+        public RestaurantService() { 
+            _restaurants = new List<Restaurant> {
+                new Restaurant {Id=1, Name="R1"},
+                new Restaurant {Id=2, Name="R2"},
+                new Restaurant {Id=3, Name="R3"}
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll() {
+            return _restaurants.OrderBy(r => r.Name);
+        }
+
+        List<Restaurant> _restaurants;
+    }
+
+    
+}
