4ef316161e931ba4c1adf94cdd6ef69680cb1ffa
dzhong <dzhong@ciena.com>
2018-06-21 09:50:36 -0400

    Set route map in controllers

--- OdeToFood/Controllers/AboutController.cs
+++ OdeToFood/Controllers/AboutController.cs
@@ -1,16 +1,22 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace OdeToFood.Controllers
 {
+
+    //[Route("controller")]
+    [Route("[controller]/[action]")]
     public class AboutController
     {
+        //[Route("action")]
         public string Phone() {
             return "111122223333";
         }
 
+        //[Route("action")]
         public string Address() {
             return "Address";
         }
