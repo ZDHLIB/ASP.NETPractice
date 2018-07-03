51d3caedb9638103541504fe53ec32cb4bc37df5
dzhong <dzhong@ciena.com>
2018-07-03 12:51:09 -0400

    Move to new folder

---
+++ NetMvc/OdeToFoodMVC/OdeToFoodMVC/Controllers/CuisineController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace OdeToFoodMVC.Controllers {
+    public class CuisineController : Controller {
+
+        public ActionResult Search(string name = "Chinese") {
+            var message = Server.HtmlEncode(name);
+            return Content(message);
+            //return RedirectPermanent("http:microsoft.com");
+            //return RedirectToAction("Index", "Home", new { name = name });
+            //return RedirectToRoute("Default", new { controller = "Home", action = "About" });
+            //return File(Server.MapPath("~/Content/site.css"), "text/css");
+            //return Json(new { Message = message, Name = "Zac" }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
