3c0838e3af6ef141cbe9e076f314bdde9273a082
dzhong <dzhong@ciena.com>
2018-07-03 13:26:21 -0400

    Add filters for action and result

---
+++ NetMvc/OdeToFoodMVC/OdeToFoodMVC/Filters/LogAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Mvc;
+
+namespace OdeToFoodMVC.Filters {
+    public class LogAttribute : ActionFilterAttribute {
+        //Execute before action
+        public override void OnActionExecuting(ActionExecutingContext filterContext) {
+            base.OnActionExecuting(filterContext);
+        }
+        //Execute after action
+        public override void OnActionExecuted(ActionExecutedContext filterContext) {
+            base.OnActionExecuted(filterContext);
+        }
+        // Execute before return result
+        public override void OnResultExecuting(ResultExecutingContext filterContext) {
+            base.OnResultExecuting(filterContext);
+        }
+        // Execute after return result
+        public override void OnResultExecuted(ResultExecutedContext filterContext) {
+            base.OnResultExecuted(filterContext);
+        }
+    }
+}
